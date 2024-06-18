b0119a425ae92fbfb1bfaf118048229aa0f7cf2d
vaishnavi saxena <119153311+Vaishnavi200603@users.noreply.github.com>
2024-06-18 22:14:59 +0530

    Add files via upload

---
+++ src/test/java/com/MakersharksAssesment/UserRegistration/UserRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MakersharksAssesment.UserRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
